db4efc4ec33ca2c288eb13c86921db0400dab53d
Shannon Houck <shouck@vt.edu>
2017-11-29 15:32:59 -0500

    Added pytest modules for 1x and S spaces

---
+++ tests/test_1x.py
@@ -0,0 +1,24 @@
+import sys, os
+sys.path.insert(1, '/usr/local/psi4/lib')
+import psi4
+sys.path.insert(1, '../')
+import spinflip
+from spinflip import sf_cas
+
+# threshold for value equality
+threshold = 1e-7
+# setting up molecule
+n2 = psi4.core.Molecule.create_molecule_from_string("""
+0 7
+N 0 0 0
+N 0 0 2.5
+symmetry c1
+""")
+
+# Test: SF-CAS/STO-3G with N2 (0,7 to 0,1)
+def test_1():
+  options = {"basis": "cc-pvdz", 'num_roots': 4}
+  e = sf_cas( 0, 1, n2, conf_space="1x", add_opts=options )
+  expected = -108.780110376393
+  assert (e - expected) < threshold
+
---
+++ tests/test_S.py
@@ -0,0 +1,24 @@
+import sys, os
+sys.path.insert(1, '/usr/local/psi4/lib')
+import psi4
+sys.path.insert(1, '../')
+import spinflip
+from spinflip import sf_cas
+
+# threshold for value equality
+threshold = 1e-7
+# setting up molecule
+o2 = psi4.core.Molecule.create_molecule_from_string("""
+0 3
+O
+O 1 1.2
+symmetry c1
+""")
+
+# Test: SF-xCIS/CC-PVDZ with O2 (0,3 to 0,1)
+def test_1():
+  options = {"basis": "cc-pvdz"}
+  e = sf_cas( 0, 1, o2, conf_space="xcis", add_opts=options )
+  expected = -149.604321051649
+  assert (e - expected) < threshold
+
