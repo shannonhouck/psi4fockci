477a333a6461825d3140f41c9096e8c87751fef1
shannonhouck <shouck@vt.edu>
2019-11-13 23:31:31 -0500

    Updated structure (packaging)

---
+++ psi4_spinflip/__init__.py
@@ -0,0 +1,11 @@
+"""
+A program for running RAS-SF-IP/EA using Psi4.
+
+
+This program runs RAS-nSF-IP/EA using Psi4's DETCI module. The 
+program is primarily run through the sf_cas function call.
+"""
+
+from .spinflip import sf_cas
+
+
