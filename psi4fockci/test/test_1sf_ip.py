3ed43c2dc6d7d1e00b4b2e4021de4f4b673e3478
shannonhouck <shouck@vt.edu>
2019-11-21 15:11:38 -0500

    Added 1SF-IP/EA tests

---
+++ psi4fockci/test/test_1sf_ip.py
@@ -0,0 +1,21 @@
+import psi4
+import psi4fockci
+from psi4fockci import sf_cas
+
+# threshold for value equality
+threshold = 1e-7
+# setting up molecule
+n2 = psi4.core.Molecule.from_string("""
+0 7
+N 0 0 0
+N 0 0 2.5
+symmetry c1
+""")
+
+# Test: CAS-1SF-IP/CC-PVDZ with N2+
+def test_1():
+  options = {"basis": "cc-pvdz"}
+  e = sf_cas( 1, 2, n2, add_opts=options, localize=True )
+  expected = -108.250639579451
+  assert (e - expected) < threshold
+
