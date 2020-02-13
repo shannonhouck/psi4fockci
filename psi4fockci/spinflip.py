d8602a7001af191955c96a83df6121770e678a44
shannonhouck <shouck@vt.edu>
2020-02-13 02:27:52 -0500

    Fixed mixed default (reduces n_det to appropriate value)

--- psi4fockci/spinflip.py
+++ psi4fockci/spinflip.py
@@ -73,6 +73,7 @@ def sf_cas( new_charge, new_multiplicity, ref_mol, conf_space="", add_opts={},
     opts = {'scf_type': 'pk',
             'basis': 'cc-pvdz',
             'reference': 'rohf',
+            'mixed': False,
             'maxiter': 1000,
             'ci_maxiter': 50 }
     opts.update(add_opts) # add additional options from user
